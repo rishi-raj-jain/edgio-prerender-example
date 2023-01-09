63c4aa57166822680ada66bb7fa3a143f5a81dcc
Rishi Raj Jain <rjain@llnw.com>
2023-01-09 14:43:06 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,16 +1,16 @@
-import { Router } from "@edgio/core/router";
+import { Router } from '@edgio/core/router'
 
 export default new Router()
-  .prerender(new Array(100).fill(0).map((i, _) => ({ path: `/${_}` })))
+  .prerender(new Array(100).fill(0).map((i, _) => ({ path: '/' + _ })))
   .fallback(({ compute, cache }) => {
     cache({
       edge: {
         maxAgeSeconds: 60 * 60 * 24 * 365,
       },
-    });
+    })
     compute((req, res) => {
-      res.body = req.url;
-      res.statusCode = 200;
-      res.statusMessage = "OK";
-    });
-  });
+      res.body = req.url
+      res.statusCode = 200
+      res.statusMessage = 'OK'
+    })
+  })
